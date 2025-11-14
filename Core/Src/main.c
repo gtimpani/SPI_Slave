dff3332456e294826ae1afb7bcc6f0646b7229cc
giorgio.timpani <giorgio.timpani@capgemini.com>
2025-11-14 13:50:35 +0100

    undone last commit

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -244,9 +244,7 @@ void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi)
 	if(hspi->Instance == SPI3)
 	{
 		volatile uint8_t index = spi3_rx_buffer[0];
-		index += (1U << 3U);
 		index >>= 3U;
-		index &= 0x7U;
 
 		spi3_tx_buffer[0] = table[index];
 		spi3_tx_buffer[1] = 0U;
